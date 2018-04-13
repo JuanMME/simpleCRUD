10e746f63fa6e8996a4f70a0fe040711d6bd1e11
Juan M. Martin Escobar <Juan@iMac-de-Juan.local>
2018-04-13 09:18:09 +0200

    added products binding to template, injected service to the component

--- app/products/product.service.ts
+++ app/products/product.service.ts
@@ -4,4 +4,24 @@ import { Product } from './product';
 @Injectable()
 export class ProductService {
 
+    // Used for testing, this is not how you would normally do it
+    getProductsFromService(): Product[] {
+        return [{
+            id: 1,
+            name: 'Scissors',
+            description: 'use this to cut stuff',
+            price: 4.99
+        }, {
+            id: 2,
+            name: 'Steak Knives',
+            description: 'use this to eat food with',
+            price: 10.99
+        }, {
+            id: 3,
+            name: 'Shot Glass',
+            description: 'use this to take shots',
+            price: 5.99
+        }]
+    }
+
 }
